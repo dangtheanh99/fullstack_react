a5c98d3f7eb4d68cf91faa55e62a4abc14ea1afa
dangtheanh99 <theanhcuacua@gmail.com>
2022-12-06 22:11:06 +0700

    init project

---
+++ src/containers/Auth/Login.js
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { push } from "connected-react-router";
+
+import * as actions from "../../store/actions";
+import "./Login.scss";
+// import { FormattedMessage } from "react-intl";
+
+// import adminService from "../services/adminService";
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="login-background">
+        <div className="login-container">
+          <div className="login-content">
+            <h2 className="login-content-heading">Login</h2>
+            <form>
+              <div class="form-group">
+                <label for="exampleInputEmail1">Username</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="exampleInputEmail1"
+                  placeholder="Enter your username"
+                />
+              </div>
+              <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input
+                  type="password"
+                  class="form-control"
+                  id="exampleInputPassword1"
+                  placeholder="Enter your password"
+                />
+              </div>
+              <button type="submit" class="btn btn-primary btn-login">
+                Log in
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    language: state.app.language,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    navigate: (path) => dispatch(push(path)),
+    adminLoginSuccess: (adminInfo) =>
+      dispatch(actions.adminLoginSuccess(adminInfo)),
+    adminLoginFail: () => dispatch(actions.adminLoginFail()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
