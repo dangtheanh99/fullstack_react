749325d063085672b504beeca02efab46d322e8e
dangtheanh99 <theanhcuacua@gmail.com>
2022-12-16 23:25:47 +0700

    video 38

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -1,33 +1,82 @@
-import React, { Component } from 'react';
-import { FormattedMessage } from 'react-intl';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { FormattedMessage } from "react-intl";
+import { connect } from "react-redux";
+import "./UserManage.scss";
+import { getAllUsers } from "../../services/userService";
+
 class UserManage extends Component {
-
-    state = {
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      allUsers: [],
+    };
+  }
+  async componentDidMount() {
+    let res = await getAllUsers("ALL");
+    console.log("get user from nodejs: ", res);
+    if (res && res.errCode === 0) {
+      this.setState({
+        allUsers: res.users,
+      });
     }
+  }
 
-    componentDidMount() {
-
-    }
-
-
-    render() {
-        return (
-            <div className="text-center">Manage users</div>
-        );
-    }
-
+  render() {
+    let { allUsers } = this.state;
+    return (
+      <div className="users-container">
+        <div className="title text-center">Manage users with The Anh</div>
+        <div className="users-table mt-3 mx-1">
+          <table id="customers">
+            <tr>
+              <th>First name</th>
+              <th>Last name</th>
+              <th>Email</th>
+              <th>Address</th>
+              <th>Actions</th>
+            </tr>
+            {allUsers &&
+              allUsers.map((item, index) => {
+                return (
+                  <tr key={index}>
+                    <td>{item.firstName}</td>
+                    <td>{item.lastName}</td>
+                    <td>{item.email}</td>
+                    <td>{item.address}</td>
+                    <td>
+                      <span
+                        className="button-edit"
+                        onClick={() => {
+                          alert("Edit user");
+                        }}
+                      >
+                        <i className="fas fa-pencil-alt"></i>
+                      </span>
+                      <span
+                        className="button-delete"
+                        onClick={() => {
+                          alert("Delete user");
+                        }}
+                      >
+                        <i className="fas fa-trash"></i>
+                      </span>
+                    </td>
+                  </tr>
+                );
+              })}
+          </table>
+        </div>
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = state => {
-    return {
-    };
+const mapStateToProps = (state) => {
+  return {};
 };
 
-const mapDispatchToProps = dispatch => {
-    return {
-    };
+const mapDispatchToProps = (dispatch) => {
+  return {};
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(UserManage);
